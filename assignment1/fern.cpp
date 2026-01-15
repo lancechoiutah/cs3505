f17c9bc204e675651ace65c6bb7640305aea02d1
lancechoiutah <lancechoiutah@gmail.com>
2026-01-15 11:46:24 -0700

    Complete

--- assignment1/fern.cpp
+++ assignment1/fern.cpp
@@ -27,7 +27,33 @@ bool getPixel(int row, int col, unsigned char* image);
 void drawImage(unsigned char* image);
 
 int main(){
+    int Size = (width * height) / 8;
+    unsigned char* image = new unsigned char[Size];
+    
+    for (int i = 0; i < Size; i++)
+    {
+        image[i] = 0;
+    }
 
+    int iterations;
+    cout << "Enter number of iterations: ";
+    std::cin >> iterations;
+
+    if (iterations <=0){
+        cout << "Iterations must be a positive integer." << endl;
+        delete[] image;
+        return 0;
+    }
+    
+    std::srand(time(0));
+
+    performIterations(iterations, image);
+
+    drawImage(image);
+
+    delete[] image;
+
+    return 0;
 }
 
 void performIterations(int iterations, unsigned char* image) {
@@ -55,25 +81,43 @@ int selectTransformation() {
 }
 
 void applyTransformation(double& x, double& y, const double* trans) {
-    double nextX = trans[0] * x + trans[1] * y + trans[4];
-    double nextY = trans[2] * x + trans[3] * y + trans[5];
+    double nextX = trans[0] * x + trans[1] * y + trans[2];
+    double nextY = trans[3] * x + trans[4] * y + trans[5];
     x = nextX;
     y = nextY;
 }
 
 void setPixel(double x, double y, unsigned char* image) {
-    int pixelX = (int)((width - 1) / 2 * (1 + x / xMax));
+    int pixelX = (int)((width - 1) / 2.0   * (1 + x / xMax));
     int pixelY = (int)((height - 1) * (1 - y / yMax));
 
     if (pixelX < 0 || pixelX >= width || pixelY < 0 || pixelY >= height) {
         return;
     }
+    int index = pixelY * width + pixelX;
+    int byteIndex = index / 8;
+    int bitIndex = index % 8;
+
+    image[byteIndex] = image[byteIndex] | (1 << bitIndex);
 }
 
 bool getPixel(int row, int col, unsigned char* image) {
-    // Fallback
+    int index = row * width + col;
+    int byteIndex = index / 8;
+    int bitIndex = index % 8;
+    return (image[byteIndex] & (1 << bitIndex)) != 0;
 }
 
 void drawImage(unsigned char* image){
+    for (int row = 0; row < height; row++) {
+        for (int col = 0; col < width; col++) {
+            if (getPixel(row, col, image)) {
+                cout << "#";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
 
 }
